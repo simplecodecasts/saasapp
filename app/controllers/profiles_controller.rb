831b8b2429cf9dd803bcf62f0b80a794e5998c31
Rob Dey <robeendey@gmail.com>
2016-07-28 07:53:41 +0000

    Added up to user edit action video

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,26 @@
 class ProfilesController < ApplicationController
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @profile = Profile.new
+  end
+  def create 
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
+    end
+  end
+  
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users do
     resource :profile
   end
   resources :contacts
-  devise_for :users, controllers: { registrations: 'users/registrations' }
 end
