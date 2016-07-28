a2bac7ea9b48a391bbfd10775b0c9e65cd64912c
Rob Dey <robeendey@gmail.com>
2016-07-28 07:18:38 +0000

    Added profiles controller and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   get 'about', to: 'pages#about'
-  
+  resources :users do
+    resource :profile
+  end
   resources :contacts
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 end
